684912f9132b00b6053441a5339d9a44cc3400e7
Алексиев <kadoll555@gmail.com>
2021-06-03 01:18:21 +0300

    built creation of spotify playlist and adding all scraped songs if found

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ import pprint
 SPOTIFY_ID = os.getenv("SPOTIFY_ID")
 SPOTIFY_KEY = os.getenv("SPOTIFY_KEY")
 redirect_URL = "http://example.com"
-# scope = "user-library-read"
+scope = "playlist-modify-private, playlist-read-private"
 
 spoty = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_ID, client_secret=SPOTIFY_KEY,
-                                                  redirect_uri=redirect_URL))
+                                                  redirect_uri=redirect_URL, scope=scope))
 billboard_base_url = "https://www.billboard.com/charts/hot-100/"
 billabord_user_selected_url = ""
 formatted_date = ""
@@ -52,3 +52,24 @@ for song in songs:
 
 pprint.pprint(songs)
 
+
+# Creates the playlist in Spotify and adds the songs from top 100:
+user_existing_playlist = [list["name"] for list in spoty.current_user_playlists()["items"]]
+if "Billboard top 100 playlist!" in user_existing_playlist:
+    print("Playlist already exists!")
+else:
+    spoty.user_playlist_create(user=spoty.current_user()["id"], public=False, name=f"Billboard top 100 playlist!")
+    print("The Playlist has been created, please check your account!")
+
+playlist_uri = [item["uri"] for item in spoty.current_user_playlists()["items"]
+                if item["name"] == "Billboard top 100 playlist!"]
+
+songs_uri_list = [uri["spotify_uri"] for uri in songs if uri["spotify_uri"] != "404 - Not Found!"]
+list_for_POST = []
+for song in songs_uri_list:
+    uri_id = song.split(":")[2]
+    list_for_POST.append(uri_id)
+
+spoty.user_playlist_add_tracks(user=spoty.current_user()["id"], playlist_id=playlist_uri[0], tracks=list_for_POST)
+
+pprint.pprint(list_for_POST)
