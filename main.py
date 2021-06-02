e2c34673199fa98d0e4f73ff869547437be610c6
Алексиев <kadoll555@gmail.com>
2021-06-02 23:54:32 +0300

    built search in spotify and songs URI extraciton

--- main.py
+++ main.py
@@ -4,41 +4,51 @@ import datetime
 import os
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+import pprint
 
 SPOTIFY_ID = os.getenv("SPOTIFY_ID")
 SPOTIFY_KEY = os.getenv("SPOTIFY_KEY")
 redirect_URL = "http://example.com"
-scope = "user-library-read"
+# scope = "user-library-read"
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_ID, client_secret=SPOTIFY_KEY,
-                                               scope=scope, redirect_uri=redirect_URL))
+spoty = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_ID, client_secret=SPOTIFY_KEY,
+                                                  redirect_uri=redirect_URL))
+billboard_base_url = "https://www.billboard.com/charts/hot-100/"
+billabord_user_selected_url = ""
+formatted_date = ""
 
-results = sp.current_user_saved_tracks()
-for idx, item in enumerate(results['items']):
-    track = item['track']
-    print(idx, track['artists'][0]['name'], " – ", track['name'])
+# Scraping Billboard site for top 100 songs:
+while type(formatted_date) == str:
+    try:
+        user_date = input("Please specify the date to get the top songs from (format must be YYYY-MM-DD): ")
+        user_date = user_date.split("-")
+        formatted_date = datetime.date(int(user_date[0]), int(user_date[1]), int(user_date[2]))
+    except ValueError:
+        print("The date you have entered is not in the right format!")
+    else:
+        billabord_user_selected_url = billboard_base_url + formatted_date.strftime("%Y-%m-%d")
 
+response = requests.get(url=billabord_user_selected_url).text
 
-# billboard_base_url = "https://www.billboard.com/charts/hot-100/"
-# billabord_user_selected_url = ""
-# formatted_date = ""
-#
-# while type(formatted_date) == str:
-#     try:
-#         user_date = input("Please specify the date to get the top songs from (format must be YYYY-MM-DD): ")
-#         user_date = user_date.split("-")
-#         formatted_date = datetime.date(int(user_date[0]), int(user_date[1]), int(user_date[2]))
-#     except ValueError:
-#         print("The date you have entered is not in the right format!")
-#     else:
-#         billabord_user_selected_url = billboard_base_url + formatted_date.strftime("%Y-%m-%d")
-#
-# response = requests.get(url=billabord_user_selected_url).text
-#
-# soup = BeautifulSoup(response, "html.parser")
-# songs = [f"{artist.text} - {song_name.text}"
-#          for artist, song_name
-#          in zip(soup.find_all(class_="chart-element__information__artist text--truncate color--secondary"),
-#                 soup.find_all(class_="chart-element__information__song text--truncate color--primary"))]
-#
-# print(songs)
+soup = BeautifulSoup(response, "html.parser")
+songs = [{"artist": artist.text, "song_name": song_name.text}
+         for artist, song_name
+         in zip(soup.find_all(class_="chart-element__information__artist text--truncate color--secondary"),
+                soup.find_all(class_="chart-element__information__song text--truncate color--primary"))]
+
+print(songs)
+
+# Searching in Spotify and update songs dict with song URI:
+results = spoty.search(q="artist:eminem track:rap god", type="track", market=None)
+pprint.pprint(results["tracks"]["items"][0]["uri"])
+
+for song in songs:
+    result = spoty.search(q=f"artist:{song['artist']} track:{song['song_name']}", type="track")
+
+    try:
+        song.update({"spotify_uri": result["tracks"]["items"][0]["uri"]})
+    except IndexError:
+        song.update({"spotify_uri": "404 - Not Found!"})
+
+pprint.pprint(songs)
+
